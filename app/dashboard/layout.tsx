889dec64ddff3b496c78ca6f6ad4ef4658d2d073
kadesignshore <kapil.designshore@gmail.com>
2026-02-18 10:24:36 +0530

    Update Next JS Changes

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from 'next';
 import SideNav from '@/app/ui/dashboard/sidenav';
- 
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+}; 
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
